48ff44a2c5552c21c19a18ec8501e63616a9dd2d
John Ntirintis <ioannis.panagiotis.diridis@gmail.com>
2024-09-18 20:53:47 +0300

    V0.2 - Gets API data and sends notif.

--- main.go
+++ main.go
@@ -3,38 +3,62 @@ package main
 import (
 	"encoding/json"
 	"fmt"
-	"io/ioutil"
+	"io"
 	"log"
+	"net/http"
 	"time"
 
 	"github.com/go-toast/toast"
 )
 
+// Constants
+// Location for API
 const pfLat = "37.9278"
 const pfLong = "23.7036"
 
 const korLat = "37.9011"
 const korLong = "23.8727"
 
+// Vars for toast notification
+const appID = "Rain Detector API"
+const title = "Rain Tomorrow!"
+const message = "Get your Jacket!"
+const iconPath = `C:\Users\giann\Downloads\cloud-rain-solid.svg`
+
 type WeatherResponse struct {
 	Daily struct {
-		PrecipitationSum []float64 `json:"precipitaion_sum"`
+		PrecipitationSum []float64 `json:"precipitation_sum"`
 		Time             []string  `json:"time"`
 	} `json:"daily"`
 }
 
-func main() {
-	pfUrl := fmt.Sprintf("https://api.open-meteo.com/v1/forecast?latitude=%s&longitude=%s&daily=precipitation_sum&timezone=auto", pfLat, pfLong)
-	korUrl := fmt.Sprintf("https://api.open-meteo.com/v1/forecast?latitude=%s&longitude=%s&daily=precipitation_sum&timezone=auto", korLat, korLong)
+var locations = []struct {
+	Latitude  string
+	Longitude string
+	Name      string
+}{
+	{"37.9278", "23.7036", "Palaio Faliro"},
+	{"37.9011", "23.8727", "Koropi"},
+}
 
-	resp, err := http.get(url)
+func main() {
+
+	pfUrl := fmt.Sprintf("https://api.open-meteo.com/v1/forecast?latitude=%s&longitude=%s&daily=precipitation_sum&timezone=auto", pfLat, pfLong)
+	//korUrl := fmt.Sprintf("https://api.open-meteo.com/v1/forecast?latitude=%s&longitude=%s&daily=precipitation_sum&timezone=auto", korLat, korLong)
+
+	actions := []toast.Action{
+		{Type: "protocol", Label: "More Info", Arguments: "https://www.example.com"},
+		{Type: "protocol", Label: "Close", Arguments: "close-app"},
+	}
+
+	resp, err := http.Get(pfUrl)
 	if err != nil {
 		fmt.Println("Error getting weather data:", err)
 		return
 	}
-	defer resp.body.Close()
+	defer resp.Body.Close()
 
-	body, _ := ioutil.ReadAll(resp.Body)
+	body, _ := io.ReadAll(resp.Body)
 
 	var weatherResp WeatherResponse
 
@@ -48,33 +72,31 @@ func main() {
 
 	for i, date := range weatherResp.Daily.Time {
 		if date == tomorrow {
-			precipitation = weatherResp.Daily.PrecipitationSum[i]
-			if precipitaion > 0 {
-				// TODO: Add params etc.
-				notification()
-				fmt.PrintLn("No rain expected tomorrow")
-			}
-			else {
-				fmt.PrintLn("No rain expected tomorrow")
+			precipitation := weatherResp.Daily.PrecipitationSum[i]
+			if precipitation > 0 {
+				toastNotification(actions)
+				fmt.Println("Rain expected tomorrow.")
+				fmt.Println("Toast notification sent.")
+			} else {
+				fmt.Println("No rain expected tomorrow")
 			}
 			break
 		}
 	}
 }
 
-func notification() {
+func toastNotification(actions []toast.Action) {
 	notification := toast.Notification{
-		AppID:   "Rain Detector API",
-		Title:   "Rain Tomorrow!",
-		Message: "Get your jacket",
-		Icon:    "C:\\Users\\giann\\Downloads\\cloud-rain-solid.svg",
-		Actions: []toast.Action{
-			{Type: "protocol", Label: "More Info", Arguments: ""},
-			{Type: "protocol", Label: "Close", Arguments: ""},
-		},
+		AppID:   appID,
+		Title:   title,
+		Message: message,
+		Icon:    iconPath,
+		Actions: actions,
 	}
 	err := notification.Push()
 	if err != nil {
+		log.Println("Error", err)
+		log.Printf("**********")
 		log.Fatalln(err)
 	}
 }
